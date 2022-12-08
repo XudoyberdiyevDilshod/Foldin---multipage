11b9d76758c74e073c06ad60151fc4db09356eb9
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2022-12-08 13:21:30 +0500

    Add 3 sections

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let elSpan = document.querySelector(".discount-sp__select");
+let elShow = document.querySelector(".dd");
+
+elSpan.addEventListener("click", function () {
+  elShow.classList.toggle("dd-show");
+});
